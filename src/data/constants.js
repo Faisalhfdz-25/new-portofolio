626d22a3c9848ef0d4268d633d9222a63f296627
FaizFaisalH <65413951+Faisalhfdz-25@users.noreply.github.com>
2024-02-22 19:20:48 +0700

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -318,33 +318,33 @@ export const Bio = {
       github: "https://github.com/rishavchanda/Trackify",
       webapp: "https://trackify.duckdns.org",
     },
-    {
-      id: 0,
-      title: "Podstream",
-      date: "Apr 2023 - May 2023",
-      description:
-        "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
-      image:
-        "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
-      tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
-      category: "web app",
-      github: "https://github.com/rishavchanda/Podstream",
-      webapp: "https://podstream.netlify.app/",
-      member: [
-        {
-          name: "Rishav Chanda",
-          img: "https://avatars.githubusercontent.com/u/64485885?v=4",
-          linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-          github: "https://github.com/rishavchanda/",
-        },
-        {
-          name: "Upasana Chaudhuri",
-          img: "https://avatars.githubusercontent.com/u/100614635?v=4",
-          linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
-          github: "https://github.com/upasana0710",
-        },
-      ],
-    },
+    // {
+    //   id: 0,
+    //   title: "Podstream",
+    //   date: "Apr 2023 - May 2023",
+    //   description:
+    //     "Developed a full-stack web application that allows users to search for, play, and pause their favorite podcasts on demand and create podcasts. Implemented user authentication using Google Auth and Jwt Auth, made responsive user interface with React JS that provides users with a seamless experience across all devices. Practiced agile methodologies to optimize team efficiency and communication.",
+    //   image:
+    //     "https://user-images.githubusercontent.com/64485885/234602896-a1bd8bcc-b72b-4821-83d6-8ad885bf435e.png",
+    //   tags: ["React Js", "MongoDb", "Node Js", "Express Js", "Redux"],
+    //   category: "web app",
+    //   github: "https://github.com/rishavchanda/Podstream",
+    //   webapp: "https://podstream.netlify.app/",
+    //   member: [
+    //     {
+    //       name: "Rishav Chanda",
+    //       img: "https://avatars.githubusercontent.com/u/64485885?v=4",
+    //       linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
+    //       github: "https://github.com/rishavchanda/",
+    //     },
+    //     {
+    //       name: "Upasana Chaudhuri",
+    //       img: "https://avatars.githubusercontent.com/u/100614635?v=4",
+    //       linkedin: "https://www.linkedin.com/in/upasana-chaudhuri-2a2bb5231/",
+    //       github: "https://github.com/upasana0710",
+    //     },
+    //   ],
+    // },
     {
       id: 1,
       title: "Vexa",
